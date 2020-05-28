27a33e194d871b306c1c27d61a52c321baf8f438
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-05-28 17:28:50 -0300

    :art: create card component

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,29 +1,64 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   position: relative;
   background: #fff;
-  border-radius: 5%;
+  width: 100%;
+  height: 8rem;
+  border-radius: 0.3rem;
   margin-bottom: 1rem;
-  padding: 1.5rem;
-  box-shadow: 0 0.1rem 0.4rem 0 rgba(192, 28, 230, 0.8);
-  border-top: 2rem solid rgba(230, 236, 245, 0.4);
+  padding: 1rem 2rem;
   cursor: grab;
 
-  header {
+  > header {
     position: relative;
-    top: -2.2rem;
-    left: 1.5rem;
   }
 
-  p {
-    font-weight: 600;
-    line-height: 2rem;
+  > footer {
+    position: absolute;
+    bottom: 1rem;
+    img {
+      vertical-align: middle;
+      width: 2.4rem;
+      border-radius: 0.12rem;
+      margin-right: 1rem;
+    }
+
+    > span {
+      font-size: 1.2rem;
+      border: 0.1rem solid rgba(190, 103, 250, 1);
+      padding: 0.25rem 0.5rem;
+      color: rgba(190, 103, 250, 1);
+    }
   }
 
-  img {
-    margin-top: 0.5rem;
-  }
+  ${(props) =>
+    props.isDragging &&
+    css`
+      border: 0.2rem dashed rgba(0, 0, 0, 0.2);
+      padding-top: 3.1rem;
+      border-radius: 0;
+      background: transparent;
+      box-shadow: none;
+      cursor: grabbing;
+      p,
+      img,
+      span,
+      header {
+        opacity: 0;
+      }
+    `}
+
+  ${(props) =>
+    props.empty &&
+    css`
+      border: 0.2rem dashed rgba(0, 0, 0, 0.1);
+      padding-top: 3.1rem;
+      border-radius: 0;
+      background: transparent;
+      box-shadow: none;
+      cursor: initial;
+    `}
 `;
 export const Label = styled.span`
   width: 1rem;

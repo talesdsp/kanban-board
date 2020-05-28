2323d07809b39c314b8b441ddac79ab4ef7ec6cd
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-05-28 17:32:01 -0300

    :sparkles: NEW: drag 'n drop cards

---
+++ src/components/Board/context.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const BoardContext = createContext({});
+export default BoardContext;
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import produce from "immer";
+import React, { useState } from "react";
+import { loadLists } from "../../services/api";
 import List from "../List";
+import BoardContext from "./context";
 import { Container } from "./styles";
 
+const data = loadLists();
+
 export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  const move = (fromList, toList, from, to) => {
+    setLists(
+      produce(lists, (draft) => {
+        const dragged = draft[fromList].cards[from];
+
+        draft[fromList].cards.splice(from, 1);
+        draft[toList].cards.splice(to, 0, dragged);
+      })
+    );
+  };
   return (
-    <Container>
-      <List />
-      <List />
-      <List />
-      <List />
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => (
+          <List key={list.title} data={list} index={index} />
+        ))}
+      </Container>
+    </BoardContext.Provider>
   );
 }
